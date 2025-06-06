cb13dbb489854b82b17cb24a66274fc6497465d3
Albin <56157868+albinpk@users.noreply.github.com>
2025-06-06 11:31:53 +0530

    chore: create `example` app

---
+++ packages/onscreen_keyboard/lib/src/onscreen_keyboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class OnscreenKeyboard extends StatelessWidget {
+  const OnscreenKeyboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('OnscreenKeyboard');
+  }
+}

f9ddb62d38e6326478925d6dc07dd1210dfceef5
Albin <56157868+albinpk@users.noreply.github.com>
2025-06-08 20:05:57 +0530

    docs: update example app with listener integration

--- packages/onscreen_keyboard/example/lib/main.dart
+++ packages/onscreen_keyboard/example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:onscreen_keyboard/onscreen_keyboard.dart';
 
@@ -11,16 +13,56 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // use OnscreenKeyboard.builder on MaterialApp.builder
       builder: OnscreenKeyboard.builder(
         width: (context) => MediaQuery.sizeOf(context).width / 2,
+        // ...more options
       ),
+
+      // or
+
+      // builder: (context, child) {
+      //   // your other codes
+      //   child = Builder(builder: (context) => child!);
+      //   // wrap with OnscreenKeyboard.wrap
+      //   return OnscreenKeyboard.wrap(child: child);
+      // },
       home: const HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final keyboard = OnscreenKeyboard.of(context);
+
+  @override
+  void initState() {
+    super.initState();
+    // listener for raw keyboard events
+    keyboard.addRawKeyDownListener(_listener);
+  }
+
+  @override
+  void dispose() {
+    keyboard.removeRawKeyDownListener(_listener);
+    super.dispose();
+  }
+
+  void _listener(OnscreenKeyboardKey key) {
+    switch (key) {
+      case TextKey(:final primary): // a text key: "a", "b", "4", etc.
+        log('key: "$primary"');
+      case ActionKey(:final name): // a action key: "shift", "backspace", etc.
+        log('action: $name');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,21 +76,30 @@ class HomeScreen extends StatelessWidget {
                 const SizedBox(height: 40),
                 TextButton(
                   onPressed: () {
+                    // open the keyboard from anywhere using
                     OnscreenKeyboard.of(context).open();
                   },
                   child: const Text('Open Keyboard'),
                 ),
                 TextButton(
                   onPressed: () {
+                    // close the keyboard from anywhere using
                     OnscreenKeyboard.of(context).close();
                   },
                   child: const Text('Close Keyboard'),
                 ),
 
-                const OnscreenKeyboardTextField(),
+                // TextField that opens the keyboard on focus
+                const OnscreenKeyboardTextField(
+                  // enableOnscreenKeyboard: false,
+                ),
+
+                // you can disable the keyboard if you want
                 const OnscreenKeyboardTextField(
                   enableOnscreenKeyboard: false,
                 ),
+
+                // a multiline TextField
                 const OnscreenKeyboardTextField(maxLines: null),
               ],
             ),

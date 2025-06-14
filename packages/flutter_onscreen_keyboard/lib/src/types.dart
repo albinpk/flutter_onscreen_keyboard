ba0d842d61e3100c20d506cab627334abf566330
Albin PK <56157868+albinpk@users.noreply.github.com>
2025-06-14 19:26:02 +0530

    feat: customizable control bar actions (#6)
    * feat: customizable control bar actions
    
    * refactor: update OnscreenKeyboard constructors
    
    - deprecate OnscreenKeyboard.wrap method
    - update error message

---
+++ packages/flutter_onscreen_keyboard/lib/src/types.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_onscreen_keyboard/src/models/keys.dart';
+
+/// A function that returns the desired width for the keyboard widget.
+typedef WidthGetter = double Function(BuildContext context);
+
+/// Signature for a listener function that responds to keyboard key events.
+///
+/// Called when a key is pressed on the on-screen keyboard.
+typedef OnscreenKeyboardListener = void Function(OnscreenKeyboardKey key);
+
+/// Signature for building a list of action widgets for the
+/// keyboard control bar.
+typedef ActionsBuilder = List<Widget> Function(BuildContext context);
